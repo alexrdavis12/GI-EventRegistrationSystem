1d9efe0a223ef401c84e277c81caf78969fca328
steevymathew <steevy.mathew@gmail.com>
2016-03-25 21:36:00 +0000

    Testing updated code

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 git add app* bin* config* db* features* lib* log* public* script* spec* tes* vendor*
-git commit --all -m "modify this comment for your commit"
+git commit --all -m "Testing updated code"
 git push
-git push heroku Alex:master
+git push heroku Steevy:master
 heroku open
