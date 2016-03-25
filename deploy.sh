ec5a281eb65e3e5a9219afc11b4e7ef2bbe64352
alexrdavis12 <alexrdavis12@gmail.com>
2016-03-25 20:53:29 +0000

    modify this comment

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-git add *
+git add app* bin* config* db* features* lib* log* public* script* spec* tes* vendor*
 git commit --all -m "modify this comment"
 git push
 git push heroku Alex:master
