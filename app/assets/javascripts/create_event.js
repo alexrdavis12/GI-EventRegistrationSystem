1ec43bcce93f7bbb591a369254ece39eefaa5c33
mdebolt <markymarc@earthlink.net>
2016-03-25 20:40:43 +0000

    button revision in 'add questions' form

--- app/assets/javascripts/create_event.js
+++ app/assets/javascripts/create_event.js
@@ -194,7 +194,7 @@ $(document).ready(function(){
                 	  <div class="form-group"></div>\
                 	  <input class="btn btn-danger" type="Button" onclick="javascript:delete_question(' + current_qid + ');"" value="Delete Question" />\
                 	  </div>';
-		$("#q_form").append("<div class='panel panel-success' id='Q" + current_qid + "'><div class='panel-heading'><h3 class='panel-title'>Question<a class='btn btn-default' style='background: none; outline:none' href='#'><i class='fa fa-caret-square-o-up fa-lg'></i></a><a class='btn btn-default' type='hidden' href='#'><i class='fa fa-caret-square-o-down fa-lg'></i></a></h3></div><div class='panel-body'><div class='form-group'><input class='form-control' name='Q" + current_qid + "[title]' value=''></div>" + q_type_str+ "</div></div></div>");
+		$("#q_form").append("<div class='panel panel-success' id='Q" + current_qid + "'><div class='panel-heading'><h3 class='panel-title'>Question<a class='btn btn-default' style='background: none; outline: 0;' href='#'><i class='fa fa-caret-square-o-up fa-lg'></i></a><a class='btn btn-default' style='background: none; outline: 0' href='#'><i class='fa fa-caret-square-o-down fa-lg'></i></a></h3></div><div class='panel-body'><div class='form-group'><input class='form-control' name='Q" + current_qid + "[title]' value=''></div>" + q_type_str+ "</div></div></div>");
 		hookClickSel();
 	});
 
