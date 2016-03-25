5ad7b4fbed815aada64365d478bb79c0ee0f7b2d
steevymathew <steevy.mathew@gmail.com>
2016-03-25 18:22:01 +0000

    test alignment

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,8 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-users = [{:email => 'info@americangimuseum.org', :lastname => 'Admin', :firstname => 'Admin',:password=>'MAGIadmin',:gender=>'',:created_at=>'',:updated_at=>'',:salt=>'$2a$10$jhA61Mm9GFSg.pcqwzJphu',:encrypted_password=>'$2a$10$jhA61Mm9GFSg.pcqwzJphuB/zwJO5nUmvIm.JfPMY15/toN2oo6jW',:level=>'0'}
-  	 ]
+users = [{:email => 'info@americangimuseum.org', :lastname => 'Admin', :firstname => 'Admin',:password=>'MAGIadmin',:gender=>'',:created_at=>'',:updated_at=>'',:salt=>'$2a$10$jhA61Mm9GFSg.pcqwzJphu',:encrypted_password=>'$2a$10$jhA61Mm9GFSg.pcqwzJphuB/zwJO5nUmvIm.JfPMY15/toN2oo6jW',:level=>'0'},
+  {:email => 'skm', :lastname => 'blah', :firstname => 'halb',:password=>'MAGIadmin',:gender=>'',:created_at=>'',:updated_at=>'',:salt=>'$2a$10$jhA61Mm9GFSg.pcqwzJphu',:encrypted_password=>'$2a$10$jhA61Mm9GFSg.pcqwzJphuB/zwJO5nUmvIm.JfPMY15/toN2oo6jW',:level=>'1'} ]
 
 users.each do |user|
   User.create!(user)
