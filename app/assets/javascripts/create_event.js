5af8234574e70da478569404b673c7f72fdb0c62
mdebolt <markymarc@earthlink.net>
2016-03-26 21:38:40 +0000

    make UI buttons beautiful

--- app/assets/javascripts/create_event.js
+++ app/assets/javascripts/create_event.js
@@ -55,7 +55,7 @@ function add_subquestion(select_qid, option_id) {
                       <option value="5">Paragraph Text</option>\
                 	  </select>\
                 	  <div class="form-group"></div>\
-                	  <input class="btn btn-danger" type="Button" onclick="javascript:delete_subquestion(' + select_qid + ', ' + option_id + ',' + cnt + ');" value="Delete Question" />\
+                	  <a class="btn btn-danger" href="javascript:delete_subquestion(' + select_qid + ', ' + option_id + ',' + cnt + ')"><i class="fa fa-trash-o fa-lg"></i> Delete Question</a>\
                 	  </div>';
     str = 	"<div class='form-group'></div><div class='panel panel-danger' id='QQ" + select_qid + "_" + option_id + "_" + cnt + "'><div class='panel-heading'><h3 class='panel-title'>Sub-question</h3></div><div class='panel-body'><div class='form-group'><input class='form-control' name='S" + select_qid + "_" + option_id + "_" + cnt + "[title]' value=''></div>" + q_type_str + "</div></div></div>";
 	$("#Q" + select_qid + "_" + option_id).append(str);
