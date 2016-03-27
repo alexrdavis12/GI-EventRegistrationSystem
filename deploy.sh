5f4cdbd5962229ce02da0c793501faae42316a4c
steevymathew <steevy.mathew@gmail.com>
2016-03-27 21:10:22 +0000

    rudimentary functions to reorder questions

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 git add app* bin* config* db* features* lib* log* public* script* spec* tes* vendor*
-<<<<<<< HEAD
 git commit --all -m "move up panel"
 git push
 git push heroku Steevy:master
-=======
-git commit --all -m "up/down buttons"
-git push
-git push heroku Marc:master
->>>>>>> master
 heroku open
