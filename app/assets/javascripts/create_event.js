0ccb2dfa670ca89a2e6c9fbd1d34b5a03b4db95e
steevymathew <steevy.mathew@gmail.com>
2016-03-27 22:10:02 +0000

    test up function

--- app/assets/javascripts/create_event.js
+++ app/assets/javascripts/create_event.js
@@ -217,7 +217,7 @@ $(document).ready(function(){
 });
 
 function move_q_up(qid){
-		window.alert($(this));
+		window.alert($(this).val());
 	var parent = $(this).parent().parent().parent();
     parent.insertBefore(parent.prev());
 }
