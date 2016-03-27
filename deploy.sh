9ba223088d845169fcbe5a189527a4772915cefe
alexrdavis12 <alexrdavis12@gmail.com>
2016-03-27 21:14:12 +0000

    checking the sync

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 git add app* bin* config* db* features* lib* log* public* script* spec* tes* vendor*
-git commit --all -m "up/down buttons"
+git commit --all -m "checking the sync"
 git push
-git push heroku Marc:master
+git push heroku Alex:master
 heroku open
