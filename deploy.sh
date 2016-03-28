492429768fb6d7bb3259446fb10e52932986cee4
alexrdavis12 <alexrdavis12@gmail.com>
2016-03-28 04:30:07 +0000

    reordering of questions and subquestions complete

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 git add app* bin* config* db* features* lib* log* public* script* spec* tes* vendor*
-git commit --all -m "testing move sub questions"
+git commit --all -m "reordering of questions and subquestions complete"
 git push
 git push heroku Alex:master
 heroku open
