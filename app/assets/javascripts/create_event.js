bb7356dcf0a528847a47fe847a4b6e0e3dc8f403
alexrdavis12 <alexrdavis12@gmail.com>
2016-03-28 03:45:38 +0000

    testing move sub questions

--- app/assets/javascripts/create_event.js
+++ app/assets/javascripts/create_event.js
@@ -49,24 +49,67 @@ function add_subquestion(select_qid, option_id) {
 	q_type_str = '<div class="form-group">\
 					  <label>Type </label>\
 					  <select class="form-control" belong="' + select_qid + '_'+ option_id + '_' + cnt +'" name="S' + select_qid + '_' + option_id + '_' + cnt +'[type]">\
-                      <option value="1">Text</option>\
-                      <option value="2">Checkboxes</option>\
-                      <option value="3">Radio Buttons</option>\
-                      <option value="4">Dropdown</option>\
-                      <option value="5">Paragraph Text</option>\
+	                      <option value="1">Text</option>\
+	                      <option value="2">Checkboxes</option>\
+	                      <option value="3">Radio Buttons</option>\
+	                      <option value="4">Dropdown</option>\
+	                      <option value="5">Paragraph Text</option>\
                 	  </select>\
                 	  <div class="form-group"></div>\
-                	  <a class="btn btn-danger" href="javascript:delete_subquestion(' + select_qid + ', ' + option_id + ',' + cnt + ')"><i class="fa fa-trash-o fa-lg"></i> Delete Sub-question</a>\
-                	  </div>';
+                	  <a class="btn btn-danger" href="javascript:delete_subquestion(' + select_qid + ', ' + option_id + ',' + cnt + ')">\
+                	  	<i class="fa fa-trash-o fa-lg"></i>\
+                	  	Delete Sub-question\
+                	  </a>\
+                  </div>';
     if($("#Q" + select_qid + "_" + option_id+"_list").length == 1)
     {
-    	 str = 	"<div class='panel panel-danger' id='QQ" + select_qid + "_" + option_id + "_" + cnt + "'><div class='panel-heading'><h3 class='panel-title'>Sub-question<a class='btn btn-sm pull-right' style='color: #3071a9; background: none; outline: 0;' href='javascript:move_subq_up("+select_qid + "_" + option_id + "_" + cnt + ")'><i class='fa fa-chevron-up fa-lg'></i></a><a class='btn btn-sm pull-right' style='color: #3071a9; background: none; outline: 0' href='javascript:move_subq_down(" + select_qid + "_" + option_id + "_" + cnt + ")'><i class='fa fa-chevron-down fa-lg'></i></a></h3></div><div class='panel-body'><div class='form-group'><input class='form-control' name='S" + select_qid + "_" + option_id + "_" + cnt + "[title]' value=''></div>" + q_type_str + "</div></div></div>";
+    	 str = 	"<div class='panel panel-danger' id='QQ" + select_qid + "_" + option_id + "_" + cnt + "'>\
+    	 			<div class='panel-heading'>\
+    	 				<h3 class='panel-title'>\
+    	 					Sub-question\
+    	 					<a class='btn btn-sm pull-right' style='color: #3071a9; background: none; outline: 0;'\
+    	 					href='javascript:move_subq_up("+select_qid + "_" + option_id + "_" + cnt + ")'>\
+    	 						<i class='fa fa-chevron-up fa-lg'/>\
+    	 					</a>\
+    	 					<a class='btn btn-sm pull-right' style='color: #3071a9; background: none; outline: 0'\
+    	 					href='javascript:move_subq_down(" + select_qid + "_" + option_id + "_" + cnt + ")'>\
+    	 						<i class='fa fa-chevron-down fa-lg'></i>\
+    	 					</a>\
+    	 				</h3>\
+    	 			</div>\
+    	 			<div class='panel-body'>\
+    	 				<div class='form-group'><input class='form-control' name='S" + select_qid + "_" + option_id + "_" + cnt + "[title]' value=''>\
+    	 				</div>\
+    	 				"+ q_type_str +"\
+    	 			</div>\
+    	 		</div>";
     	 $("#Q" + select_qid + "_" + option_id+"_list").append(str);
     	 hookClickSel();
     }
     else
     {
-    	str = 	"<li id='Q" + select_qid + "_" + option_id+"_list' style='list-style-type:none'><div class='panel panel-danger' id='QQ" + select_qid + "_" + option_id + "_" + cnt + "'><div class='panel-heading'><h3 class='panel-title'>Sub-question<a class='btn btn-sm pull-right' style='color: #3071a9; background: none; outline: 0;' href='javascript:move_subq_up(" + select_qid + "_" + option_id + "_" + cnt + ")'><i class='fa fa-chevron-up fa-lg'></i></a><a class='btn btn-sm pull-right' style='color: #3071a9; background: none; outline: 0' href='javascript:move_subq_down(" + select_qid + "_" + option_id + "_" + cnt + ")'><i class='fa fa-chevron-down fa-lg'></i></a></h3></div><div class='panel-body'><div class='form-group'><input class='form-control' name='S" + select_qid + "_" + option_id + "_" + cnt + "[title]' value=''></div>" + q_type_str + "</div></div></div></li>";
+    	str = 	"<li id='Q" + select_qid + "_" + option_id+"_list' style='list-style-type:none'>\
+    				<div class='panel panel-danger' id='QQ" + select_qid + "_" + option_id + "_" + cnt + "'>\
+	    				<div class='panel-heading'>\
+	    					<h3 class='panel-title'>\
+	    						Sub-question\
+	    						<a class='btn btn-sm pull-right' style='color: #3071a9; background: none; outline: 0;'\
+	    						href='javascript:move_subq_up(" + select_qid + "_" + option_id + "_" + cnt + ")'>\
+	    							<i class='fa fa-chevron-up fa-lg'/>\
+	    						</a>\
+	    						<a class='btn btn-sm pull-right' style='color: #3071a9; background: none; outline: 0'\
+	    						href='javascript:move_subq_down(" + select_qid + "_" + option_id + "_" + cnt + ")'>\
+	    							<i class='fa fa-chevron-down fa-lg'/>\
+	    						</a>\
+	    					</h3>\
+	    				</div>\
+    					<div class='panel-body'>\
+    						<div class='form-group'>\
+    							<input class='form-control' name='S" + select_qid + "_" + option_id + "_" + cnt + "[title]' value=''>\
+    						</div>" + q_type_str + "\
+    					</div>\
+    				</div>\
+    			</li>";
     	$("#Q" + select_qid + "_" + option_id).append(str);	
     	hookClickSel();
     }
@@ -196,20 +239,39 @@ $(document).ready(function(){
 		evt.stopImmediatePropagation();
 		current_qid += 1;
 		q_type_str = '<div class="form-group">\
-					  <label>Type</label>\
-					  <select class="form-control" belong=' + current_qid + ' name="Q' + current_qid + '[type]">\
-                      <option value="1">Text</option>\
-                      <option value="2">Checkboxes</option>\
-                      <option value="3">Radio Buttons</option>\
-                      <option value="4">Dropdown</option>\
-                      <option value="5">Paragraph Text</option>\
-                	  </select>\
-                	  <div class="form-group"></div>\
-                	  <a class="btn btn-danger" href="javascript:delete_question(' + current_qid + ')">\
-  						<i class="fa fa-trash-o fa-lg"></i>\
-  						Delete Question\
-					  </a>';
-		$("#q_list").append("<div class='panel panel-success' id='Q" + current_qid + "'><div class='panel-heading'><h3 class='panel-title'>Question<a class='btn btn-sm pull-right' style='color: #3071a9; background: none; outline: 0;' href='javascript:move_q_up(" + current_qid + ")'><i class='fa fa-chevron-up fa-lg'></i></a><a class='btn btn-sm pull-right' style='color: #3071a9; background: none; outline: 0' href='javascript:move_q_down(" + current_qid + ")'><i class='fa fa-chevron-down fa-lg'></i></a></h3></div><div class='panel-body'><div class='form-group'><input class='form-control' name='Q" + current_qid + "[title]' value=''></div>" + q_type_str+ "</div></div></div>");
+						  <label>Type</label>\
+						  <select class="form-control" belong=' + current_qid + ' name="Q' + current_qid + '[type]">\
+		                      <option value="1">Text</option>\
+		                      <option value="2">Checkboxes</option>\
+		                      <option value="3">Radio Buttons</option>\
+		                      <option value="4">Dropdown</option>\
+		                      <option value="5">Paragraph Text</option>\
+	                	  </select>\
+	                	  <div class="form-group"></div>\
+	                	  <a class="btn btn-danger" href="javascript:delete_question(' + current_qid + ')">\
+	  						<i class="fa fa-trash-o fa-lg"></i>\
+	  						Delete Question\
+						  </a>\
+						</div>';
+		$("#q_list").append("<div class='panel panel-success' id='Q" + current_qid + "'>\
+								<div class='panel-heading'>\
+									<h3 class='panel-title'>\
+										Question\
+										<a class='btn btn-sm pull-right' style='color: #3071a9; background: none; outline: 0;'\
+										href='javascript:move_q_up(" + current_qid + ")'>\
+											<i class='fa fa-chevron-up fa-lg'></i>\
+										</a>\
+										<a class='btn btn-sm pull-right' style='color: #3071a9; background: none; outline: 0'\
+										href='javascript:move_q_down(" + current_qid + ")'>\
+											<i class='fa fa-chevron-down fa-lg'></i>\
+										</a>\
+									</h3>\
+								</div>\
+								<div class='panel-body'>\
+									<div class='form-group'><input class='form-control' name='Q" + current_qid + "[title]' value=''></div>\
+									" + q_type_str+ "\
+								</div>\
+							</div>");
 		hookClickSel();
 	});
 
