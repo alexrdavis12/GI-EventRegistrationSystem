60aa2cda167b6c56733d44f5505f093b1fa78b1d
mdebolt <markymarc@earthlink.net>
2016-03-28 23:40:25 +0000

    make UI akin to Alex's version

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 git add app* bin* config* db* features* lib* log* public* script* spec* tes* vendor*
-git commit --all -m "make UI buttons beautiful"
+git commit --all -m "make UI akin to Alex's version"
 git push
 git push heroku Marc:master
 heroku open
