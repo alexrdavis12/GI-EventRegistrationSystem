dc182304014b88f2ea5004bda0e5f737cfd6fe18
steevymathew <steevy.mathew@gmail.com>
2016-03-28 20:45:05 +0000

    check sync master works

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,5 @@
 git add app* bin* config* db* features* lib* log* public* script* spec* tes* vendor*
 git commit --all -m "check sync master works"
 git push
-git push heroku Steevy:master
+git push heroku master
 heroku open
