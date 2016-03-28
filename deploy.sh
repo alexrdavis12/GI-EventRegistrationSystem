da986d595c0329110987cf78521e32e2b04d675b
Dee <deyanaw9@gmail.com>
2016-03-28 23:07:52 +0000

    testing dev environment

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 git add app* bin* config* db* features* lib* log* public* script* spec* tes* vendor*
-git commit --all -m "button differences"
+git commit --all -m "testing dev environment"
 git push
-git push heroku master
+git push heroku Deyana:master
 heroku open
