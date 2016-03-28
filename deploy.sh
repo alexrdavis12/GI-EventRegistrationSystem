a1f1f8be60d02168e9232a30a4470a0b804bf259
steevymathew <steevy.mathew@gmail.com>
2016-03-28 18:59:06 +0000

    cucumber tests

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 git add app* bin* config* db* features* lib* log* public* script* spec* tes* vendor*
-git commit --all -m "check sync master works"
+git commit --all -m "cucumber tests"
 git push
 git push heroku Steevy:master
 heroku open
