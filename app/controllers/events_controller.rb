16c723fa823f14a782d839cf4562737f82fa764a
kathryn02 <kathryn02@tamu.edu>
2016-03-28 20:35:39 +0000

    commit

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -34,7 +34,7 @@ class EventsController < ApplicationController
     end
   
     def edit
-      @event = Event.find params[:id]
+      @eventedit = Event.find(eid)
     end
     
     def allevent
