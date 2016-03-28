b07726da1a8d1d25de72c30ba39f8ac4f73db663
alexrdavis12 <alexrdavis12@gmail.com>
2016-03-28 06:10:31 +0000

    button differences

--- app/assets/javascripts/create_event.js
+++ app/assets/javascripts/create_event.js
@@ -109,7 +109,7 @@ function add_subquestion(select_qid, option_id) {
     					</div>\
     				</div>\
     			</li>";
-    	$("#Q" + select_qid + "_" + option_id).before(str);	
+    	$("#Q" + select_qid + "_" + option_id).after(str);	
     	hookClickSel();
     }
 	
