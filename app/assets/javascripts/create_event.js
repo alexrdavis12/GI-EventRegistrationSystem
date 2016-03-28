aefc3222fbb31fda78c5525ba2e818762168c333
alexrdavis12 <alexrdavis12@gmail.com>
2016-03-28 18:11:02 +0000

    testing

--- app/assets/javascripts/create_event.js
+++ app/assets/javascripts/create_event.js
@@ -24,7 +24,7 @@ function add_optionsforsub(qid, option_id, subqcnt) {
 	cnt = $("#Q" + qid + "_" + option_id + "_" + subqcnt + "_optcnt").val();
 	$("#Q" + qid + "_" + option_id + "_" + subqcnt + "_optcnt").val(++cnt);
 	$("#Q" + qid + "_" + option_id + "_" + subqcnt + "_insertpos").before("<div class='form-inline'  id='Q" + qid+ "_" + option_id + "_" + subqcnt + "_" + cnt + "'																			>\
-																				<div class='form-group width='80%''>\
+																				<div class='col-xs-7'>\
 																				<input class='form-control'  name='S" + qid + "_" + option_id + "_" + subqcnt + "[opt" + cnt + "]' value=''/>\
 																				</div>\
 																				<a class='btn btn-danger pull-right' href='javascript:delete_optionforsub("+ qid + "," + option_id + "," + subqcnt + "," + cnt + ")'>\
