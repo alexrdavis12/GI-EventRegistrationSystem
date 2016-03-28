39ffaf7f9909ac07b98126cbb69f73703209c969
alexrdavis12 <alexrdavis12@gmail.com>
2016-03-28 00:54:12 +0000

    testing move sub questions

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 git add app* bin* config* db* features* lib* log* public* script* spec* tes* vendor*
-git commit --all -m "check sync master works"
+git commit --all -m "testing move sub questions"
 git push
 git push heroku Alex:master
 heroku open
