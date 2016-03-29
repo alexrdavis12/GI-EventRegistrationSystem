cf1771a476a5f48c1be3cde3956be53506c255de
Dee <deyanaw9@gmail.com>
2016-03-29 00:48:01 +0000

    testing dev environment

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -33,6 +33,11 @@ class EventsController < ApplicationController
       eid=params[:eid]
     	@event = Event.find(eid)
     end
+    
+    def edit
+      @event = Event.find params[:id]
+    end
+    
     	
     def allevent
       id = session[:user_id]
