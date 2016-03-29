219bbc50e57b0081a72bc3de70f39e0ba0158d92
alexrdavis12 <alexrdavis12@gmail.com>
2016-03-29 03:31:22 +0000

    testing

--- app/assets/javascripts/create_event.js
+++ app/assets/javascripts/create_event.js
@@ -38,7 +38,7 @@ function add_options(qid) {
 	cnt = $("#Q" + qid + "_optcnt").val();
 	$("#Q" + qid + "_optcnt").val(++cnt);
 	$("#Q" + qid + "_options_list").append("<div class='form-group' id='Q" + qid+ "_" + cnt + "'>\
-											 <input class='form-control' name='Q" + qid + "[opt" + cnt + "]' value=''>\
+											 <input class='form-control' id='Q" + qid + "_O"+cnt+"' name='Q" + qid + "[opt" + cnt + "]' value=''>\
 											 <div class='form-group'></div>\
 											 <a class='btn btn-danger pull-right' href='javascript:delete_option("+ qid + "," + cnt + ")' value='vis'>\
 											 	<i class='fa fa-trash-o fa-lg'></i>\
@@ -122,7 +122,7 @@ function add_subquestion(select_qid, option_id) {
     					</div>\
     				</div>\
     			</li>";
-    	$("#Q" + select_qid + "_" + option_id).append(str);	
+    	$("#Q" + select_qid + "_O" + option_id).append(str);	
     	hookClickSel();
     }
 	
@@ -156,7 +156,7 @@ function trigger_option(dom_obj, select_qid, displaysub) {
 							 	<li id='Q" + select_qid + "_options_list'>\
 									 <input id='Q" + select_qid + "_optcnt' type='hidden' value='3'>\
 									 <div class='form-group' id='Q" + select_qid + "_1'>\
-			   							<input class='form-control' name='Q" + select_qid + "[opt1]' value=''/>\
+			   							<input class='form-control' id='Q" + select_qid + "_O1' name='Q" + select_qid + "[opt1]' value=''/>\
 		   							 	<div class='form-group'></div>\
 		   							 	<a class='btn btn-success' href='javascript:add_subquestion(" + select_qid + ", 1)'>\
    							 				<i class='fa fa-plus fa-lg'></i>\
