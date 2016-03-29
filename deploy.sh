f1d441b5020ecbdd9bcfec87ac81f44361de7e30
mdebolt <markymarc@earthlink.net>
2016-03-29 08:21:37 +0000

    testing

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 git add app* bin* config* db* features* lib* log* public* script* spec* tes* vendor*
-<<<<<<< HEAD
-git commit --all -m "testing functionality"
-=======
 git commit --all -m "testing"
->>>>>>> Alex
 git push
-git push heroku Steevy:master
+git push heroku Marc:master
 heroku open
