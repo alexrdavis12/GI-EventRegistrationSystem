0f3f61d68fab9843ea00eb62762c71c6f9c47745
alexrdavis12 <alexrdavis12@gmail.com>
2016-03-29 01:11:28 +0000

    testing

--- app/assets/javascripts/create_event.js
+++ app/assets/javascripts/create_event.js
@@ -118,7 +118,7 @@ function add_subquestion(select_qid, option_id) {
     					</div>\
     				</div>\
     			</li>";
-    	$("#Q" + select_qid + "_" + option_id).append(str);	
+    	$("#Q" + select_qid + "_" + option_id).before(str);	
     	hookClickSel();
     }
 	
