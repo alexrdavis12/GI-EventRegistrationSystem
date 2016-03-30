6707515f9c471586bf774201df7a79ef6b1ca610
kathryn02 <kathryn02@tamu.edu>
2016-03-30 20:16:32 +0000

    testing functionality

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 git add app* bin* config* db* features* lib* log* public* script* spec* tes* vendor*
-<<<<<<< HEAD
-git commit --all -m "adding id to up and down arrows"
-=======
 git commit --all -m "testing functionality"
->>>>>>> 163d5acbcc2b0918c79168b032879b0e3aa80df6
 git push
 git push heroku kathryn:master
 heroku open
