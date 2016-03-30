86e3c2e14307831902aae5b1b3df7c1f0a78690f
steevymathew <steevy.mathew@gmail.com>
2016-03-30 23:07:17 +0000

    testing functionality

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,5 @@
 git add app* bin* config* db* features* lib* log* public* script* spec* tes* vendor*
 git commit --all -m "testing functionality"
 git push
-git push heroku kathryn:master
+git push heroku Steevy:master
 heroku open
