307818bb78b57246cfd815dd4a195bf17ba185be
steevymathew <steevy.mathew@gmail.com>
2016-03-30 00:57:16 +0000

    adding id to up and down arrows

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,5 @@
 git add app* bin* config* db* features* lib* log* public* script* spec* tes* vendor*
 git commit --all -m "adding id to up and down arrows"
 git push
-git push heroku Alex:master
+git push heroku Steevy:master
 heroku open
