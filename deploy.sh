786ecdde26a9fedb7d79882eef79d016f6d584aa
kathryn02 <kathryn02@tamu.edu>
2016-03-30 04:39:59 +0000

    adding id to up and down arrows

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 git add app* bin* config* db* features* lib* log* public* script* spec* tes* vendor*
-<<<<<<< HEAD
 git commit --all -m "adding id to up and down arrows"
-=======
-git commit --all -m "adding id to up and down arrows"
->>>>>>> master
 git push
 git push heroku kathryn:master
 heroku open
