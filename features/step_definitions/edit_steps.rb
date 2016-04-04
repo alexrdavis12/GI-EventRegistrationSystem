fbeeeb090b3daf363c664d67c115158dcf26e6f0
Dee <Deyana033@users.noreply.github.com>
2016-04-04 18:57:25 -0500

    Update edit_steps.rb

--- features/step_definitions/edit_steps.rb
+++ features/step_definitions/edit_steps.rb
@@ -1,3 +1,11 @@
+When(/^I go to the Event Page%/)do
+  visit()
+end
+
+Then(/^I should see events%/)do
+  expect(page).to have_content("Event")
+end
+
 Given /^(?:|I )am on the event page (.+)$/ do |page_name|
   visit path_to(page_name)
 end
