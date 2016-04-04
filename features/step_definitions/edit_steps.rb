6cb72d231db2296326dbe528f00d6c7915397ae5
Dee <Deyana033@users.noreply.github.com>
2016-04-04 18:50:58 -0500

    Update edit_steps.rb

--- features/step_definitions/edit_steps.rb
+++ features/step_definitions/edit_steps.rb
@@ -1,17 +1,14 @@
-Given(/^I am on the event page$/) do
-  visit""
-end 
-
-When(/^I click edit$/)do
-  find_link("edit").click
+Given /^(?:|I )am on the event page (.+)$/ do |page_name|
+  visit path_to(page_name)
 end
 
-When(/^I fill in "(.*?)" with "(.*?)"$/) do |field,text|
+When /^(?:|I )click edit"([^"]*)"$/ do |button|
+  click_button(button)
+end
+
+Then (/^I edit  "(.*?)" with "(.*?)"$/) do |field,text|
   fill_in(field, with:text)
 end
 
-When(/^I click the "(.*?)" button$/) do |arg1|
-  click_button(arg1)
-end
 
-Then(/^I should see it change to the event form with previous information already in field)
+
