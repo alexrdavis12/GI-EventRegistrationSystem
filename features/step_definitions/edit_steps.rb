6b19c3d9ed56141a69b9de3e2f94f4ce5b8090a0
Dee <Deyana033@users.noreply.github.com>
2016-04-11 13:04:57 -0500

    Update edit_steps.rb

--- features/step_definitions/edit_steps.rb
+++ features/step_definitions/edit_steps.rb
@@ -3,29 +3,31 @@ Given /^(?:|I )am on the Event Page (.+)$/ do |page_name|
 end
 
 And(/^There are more than one event on the page$/) do
+   #visit('EventCreate?eid=1')
+   page.find("a", text:"Edit").click
    pending # express the regexp above with the code you wish you had
-  pending #visit('EventCreate?eid=1')
-  pending #page.find("a", text:"Edit").click
+
 end
 
 When /^(?:|I )click Edit"([^"]*)"$/ do |button|
- pending # express the regexp above with the code you wish you had
-  pending # click_button(button)
+   click_button(button)
+   pending # express the regexp above with the code you wish you had
 end
 
 Then (/^I should see the event form with event information "(.*?)" with "(.*?)"$/) do |field,text|
- pending # express the regexp above with the code you wish you had
-  pending #fill_in(field, with:text)
+   fill_in(field, with:text)
+   pending # express the regexp above with the code you wish you had
+     
 end
 
 When /^(?:|I )click Edit Question"([^"]*)"$/ do |button|
-  pending # express the regexp above with the code you wish you had
-  pending #click_button(button)
+   click_button(button)
+   pending # express the regexp above with the code you wish you had
 end
 
 Then (/^I should see the event form with question information "(.*?)" with "(.*?)"$/) do |field,text|
+   fill_in(field, with:text)
    pending # express the regexp above with the code you wish you had
-  pending #fill_in(field, with:text)
 end
 
 
