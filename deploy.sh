95cd70b1efd2fe842b52aaacb37af661d2a4c60c
Alex <alexrdavis12@gmail.com>
2016-04-11 20:31:39 +0000

    Demo for client

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 git add app* bin* config* db* features* lib* log* public* script* spec* tes* vendor*
-git commit --all -m "User Profile: Vehicle UI works"
+git commit --all -m "Demo for client"
 git push
-git push heroku Steevy:master
+git push -f heroku master
 heroku open
