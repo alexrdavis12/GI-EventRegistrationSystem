eb92b005f26a255fcb412d9caae9bff87351ac94
Dee <deyanaw9@gmail.com>
2016-04-12 18:55:52 +0000

    commit message

--- features/step_definitions/edit_steps.rb
+++ features/step_definitions/edit_steps.rb
@@ -1,36 +1,39 @@
-Given /^(?:|I )am on the "(.+)" page$/ do |page_name|
-   pending # express the regexp above with the code you wish you had visit path_to(page_name)
+Given (/^(?:|I )am on the Event page$/) do 
+   visit root_path
 end
 
-And(/^There are more than one event on the page$/) do
-   #visit('EventCreate?eid=1')
-   page.find("a", text:"Edit").click
-   pending # express the regexp above with the code you wish you had
+And(/^There are more than one event on the page$/) do 
+   visit('eventedit?eid=')
+   find('input [name="edit"]')
+   #find("a", text:"edit").click
+end
+Then(/^I should see Events $/) do 
+ # xpect(page).to have_content("edit")
+ 
 end
 
-When (/^(?:|I )click Edit"([^"]*)"$/) do |button| 
-   click_button(button)
-   pending # express the regexp above with the code you wish you had
+When (/^(?:|I) click Edit$/) do
+  
+  
 end
 
-Then (/^I should see the event form with event information "(.*?)" with "(.*?)"$/) do |field,text|
-   fill_in(field, with:text)
-   pending # express the regexp above with the code you wish you had
+Then (/^I should see it change to the event form with event information$/) do
+
+end
+  
+Given (/^(?:|I )am on the Event Question$/) do 
+   visit('QuestionEdit?eid=1')
+   #find(text: "Edit").click
+   click_button("edit")
+  
 end
 
-Given /^(?:|I )am on the Event Question (.+)$/ do |page_name|
-   #pending
+When (/^(?:|I )click Edit Question$/) do 
+   
 end
 
-When /^(?:|I )click Edit Question"([^"]*)"$/ do |button|
-   click_button(button)
-   pending # express the regexp above with the code you wish you had
+Then (/^I should see it change to the edit form with question information$/) do
+
 end
 
-Then (/^I should see the event form with question information "(.*?)" with "(.*?)"$/) do |field,text|
-   fill_in(field, with:text)
-   pending # express the regexp above with the code you wish you had
-end
-
-
-
+ 
