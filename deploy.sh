84548b6e6e54d69d0d12382fd8c0f94eefd445c2
mdebolt <markymarc@earthlink.net>
2016-04-12 07:50:54 +0000

    edit questions and user homepage work

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 git add app* bin* config* db* features* lib* log* public* script* spec* tes* vendor*
-git commit --all -m "upating heroku in prep to update master"
+git commit --all -m "edit questions and user homepage work"
 git push
 git push -f heroku Marc:master
 heroku open
