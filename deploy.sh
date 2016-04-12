e424318802f90624bf399f5fb631b0a1557249a7
Alex <alexrdavis12@gmail.com>
2016-04-12 00:48:30 +0000

    damn dev log

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,5 @@
 git add app* bin* config* db* features* lib* log* public* script* spec* tes* vendor*
 git commit --all -m "damn dev log"
 git push
-git push -f heroku Alex:master
+git push heroku Alex:master
 heroku open
