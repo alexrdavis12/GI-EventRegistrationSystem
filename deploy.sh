577afc827c1041474a22a347fecc9aa88110364c
kathryn02 <kathryn02@tamu.edu>
2016-04-12 02:38:08 +0000

    edit description of event works

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 git add app* bin* config* db* features* lib* log* public* script* spec* tes* vendor*
-git commit --all -m "testing edit question functionality"
+git commit --all -m "edit description of event works"
 git push
 git push heroku kathryn:master
 heroku open
