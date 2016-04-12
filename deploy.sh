c519b919854e26b975c36d05bf18058a3db96892
kathryn02 <kathryn02@tamu.edu>
2016-04-12 05:05:26 +0000

    got rid of number column on questions/update

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 git add app* bin* config* db* features* lib* log* public* script* spec* tes* vendor*
-git commit --all -m "can correctly add, delete, and move questions on edit"
+git commit --all -m "got rid of number column on questions/update"
 git push
 git push heroku kathryn:master
 heroku open
