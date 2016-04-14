f37a3b74888137d3c766612455b78c9bfd7c94c3
mdebolt <markymarc@earthlink.net>
2016-04-14 00:24:26 +0000

    testing vehicle auto-populates

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,5 @@
 git add app* bin* config* db* features* lib* log* public* script* spec* tes* vendor*
 git commit --all -m "testing vehicle auto-populates"
 git push
-git push heroku Marc:master
+git push -f heroku Marc:master
 heroku open
