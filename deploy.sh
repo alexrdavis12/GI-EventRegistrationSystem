32674e96b582cce39a0089cb3eb8ce03ec1f1027
kathryn02 <kathryn02@tamu.edu>
2016-04-14 00:17:47 +0000

    profile tests

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,5 @@
 git add app* bin* config* db* features* lib* log* public* script* spec* tes* vendor*
 git commit --all -m "profile tests"
 git push
-git push heroku Steevy:master
+git push heroku master
 heroku open
