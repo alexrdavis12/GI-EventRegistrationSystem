e5c49ea7c688f82c1e539d627c683bc19741c334
kathryn02 <kathryn02@tamu.edu>
2016-04-14 00:12:57 +0000

    just a c9 issue?

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 git add app* bin* config* db* features* lib* log* public* script* spec* tes* vendor*
-git commit --all -m "profile tests"
+git commit --all -m "just a c9 issue?"
 git push
-git push heroku Steevy:master
+git push heroku kathryn02:master
 heroku open
