47208138365ef2162957608fc939b5d234220827
kathryn02 <kathryn02@tamu.edu>
2016-04-19 00:13:12 +0000

    added fields show correctly on data table

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 git add app* bin* config* db* features* lib* log* public* script* spec* tes* vendor*
-git commit --all -m "added phone number field"
+git commit --all -m "added fields show correctly on data table"
 git push
 git push heroku kathryn02:master
 heroku open
