7528d07251e67613055ed609df50aedb81799360
mdebolt <markymarc@earthlink.net>
2016-04-21 00:59:11 +0000

    masterful work

--- db/migrate/20160419142111_add_address.rb
+++ db/migrate/20160419142111_add_address.rb
@@ -1,6 +1,5 @@
 class AddAddress < ActiveRecord::Migration
   def change
-    remove_column :users, :state, :string
     add_column :users, :addressline1, :string
     add_column :users, :city, :string
     add_column :users, :state, :string
