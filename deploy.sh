1bb49576a1277e045a1e1f352dff425879b293d4
mdebolt <markymarc@earthlink.net>
2016-05-03 00:14:45 +0000

    Editing Edit functionality

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 git add app* bin* config* db* features* lib* log* public* script* spec* tes* vendor*
-git commit --all -m "Test Iteration 3"
+git commit --all -m "Editing Edit functionality"
 git push
 git push heroku Marc:master
 heroku open
