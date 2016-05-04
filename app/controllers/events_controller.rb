858991c9be2bbd11aca776d817282a92ba1124d6
steevymathew <steevy.mathew@gmail.com>
2016-05-04 00:34:31 +0000

    sync

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -113,16 +113,16 @@ class EventsController < ApplicationController
       	end
     end
   end 
-
+end 
   
   def allevent
     id = session[:user_id]
     @curlevel = User.find(id).level
     if(@curlevel != 0)
-    @event = Event.all
+      @event = Event.all
     else
       redirect_to '/admin'
     end
   end
-end 
+
 end 
